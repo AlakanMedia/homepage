dce8cd0aecad7ef15826c2bb806828d5cdfb17f0
AlakanMedia <alakan439@gmail.com>
2024-07-20 22:24:05 -0500

    A search bar has been added

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-let time = document.getElementById("time");
-let date = document.getElementById("date");
+// =======================================================
+// Función para la presentar la fecha y hora
+// =======================================================
+const time = document.getElementById("time");
+const date = document.getElementById("date");
 
 const month_names = [
     "January", "February", "March", "April",
@@ -17,3 +20,27 @@ let interval = setInterval(() => {
     time.innerHTML = local_date.toLocaleTimeString();
     date.innerHTML = `${day} ${month_names[month]} ${year}`;
 }, 1000);
+
+// =======================================================
+// Función para manejar el ancho de la barra de búsqueda
+// =======================================================
+const input = document.getElementById("search_text");
+let initial_width = input.placeholder.length; 
+
+input.style.width = initial_width + "ch";
+
+input.addEventListener("input", function() {
+    let desired_width = this.value.length;
+    const max_width = 18;
+
+    if (desired_width == 0) {
+        input.placeholder = "type...";
+        desired_width = initial_width;
+    }
+
+    if (desired_width > max_width) {
+        desired_width = max_width;
+    }
+
+    this.style.width = desired_width + "ch";
+});
